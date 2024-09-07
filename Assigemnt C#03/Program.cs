128466d50a69790d5c38bfefad0d7086e187f8ef
MohamedAbdElhamed7 <mohamedabdalhamed845@gmail.com>
2024-09-07 09:01:27 +0300

    Question 1

--- Assigemnt C#03/Program.cs
+++ Assigemnt C#03/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 1
+            //1- Write a program that takes a number from the user then print
+            //yes if that number can be divided by 3 and 4 otherwise print no.
+            //Console.WriteLine("Eneter The Number ");
+            //int Number = int.Parse(Console.ReadLine());
+
+            //if (Number % 3 == 0 && Number % 4 == 0)
+            //    Console.WriteLine("yes");
+            //else 
+            //    Console.WriteLine("no");
+            
+
+            #endregion
+
         }
     }
 }
