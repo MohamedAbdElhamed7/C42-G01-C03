b390b757a455010aac099584a25a728de424a16f
MohamedAbdElhamed7 <mohamedabdalhamed845@gmail.com>
2024-09-07 09:54:10 +0300

    Question 2

--- Assigemnt C#03/Program.cs
+++ Assigemnt C#03/Program.cs
@@ -14,7 +14,22 @@
             //    Console.WriteLine("yes");
             //else 
             //    Console.WriteLine("no");
-            
+
+
+            #endregion
+
+            #region Question 2
+            //Q2.Write a program that allows the user to insert an integer
+            //then print negative if it is negative number otherwise print positive.
+
+            //Console.WriteLine("Please Eneter an Integre Number ");
+            //int Number = int.Parse(Console.ReadLine());
+
+            //if (Number < 0)
+            //    Console.WriteLine("negative");
+            //else
+            //    Console.WriteLine("positive");
+
 
             #endregion
 
