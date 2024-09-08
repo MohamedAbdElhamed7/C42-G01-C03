d2234a635519fcb27b3a83caaf8f9d73f2f43d01
MohamedAbdElhamed7 <mohamedabdalhamed845@gmail.com>
2024-09-08 17:32:10 +0300

    Question 22

--- Assigemnt C#03/Program.cs
+++ Assigemnt C#03/Program.cs
@@ -1,10 +1,12 @@
-﻿using System.ComponentModel;
-using System.Reflection.Metadata;
-using System.Security.Cryptography;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿
 
 namespace Assigemnt_C_03
 {
+    class Point
+    {
+        public int x;
+        public int y;
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -242,10 +244,50 @@ namespace Assigemnt_C_03
             //else
             //    Console.WriteLine("You have to leave the company");
 
+            #endregion
+
+            #region Question 21
+            //Q21.Write C# program that Assigning one value type variable to another and
+            //modifying the value of one variable and mention what will happen
+
+
+
+            //int x = 5;
+            //int y = 8;
+
+            //x = y;
+            //so x will be equals to 8
+
+            //y = 3;
+            // so y will be equal 3 but x will still equals to 8
+            // becouse x = y thats mean that x equals to the  value of y (8) not equal to the y
+            // so x will not change from 8 to 3 
+
+            //Console.WriteLine($"The value of x is : {x}");
+            //Console.WriteLine($"The value of y is : {y}");
 
 
             #endregion
 
+            #region Question 22
+            //Q3.Write C# program that Assigning one reference type variable to another
+            //and modifying the object through one variable and mention what will happen
+
+
+            //Point P1;
+            // Declare for reference from point , refer null 
+            //Allocate 4 bytes at stack
+            //P1 = new Point();
+            // allocate require bytes at heap , 8bytes 
+            //P1.x = 1;
+            //P1.y = 2;
+
+            //Point P2 = new Point(); // unreachable object 
+            //P2 = P1;
+
+            //if there is change at value of p2 p1 will have same change also if p1 have changes
+            //p2 will have  the same because p1 and p2 refere for same location at heap  
+            #endregion
         }
     }
 }
